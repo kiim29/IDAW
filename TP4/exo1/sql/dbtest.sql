80743af69ed678ef1ce8c3e3922ec8739153a88a
Kim Luxembourger <kim.luxembourger@etu.imt-nord-europe.fr>
2023-03-20 10:30:15 +0100

    TP4 - exo1 et exo2

---
+++ TP4/exo1/sql/dbtest.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 20 mars 2023 à 08:49
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `dbtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'lulu', 'lucas@grrr.f'),
+(2, 'stacy', 'srey@mager.c'),
+(3, 'mackleroy', 'mlkbr@protonprotonproton.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ TP4/exo2/users.php
@@ -0,0 +1,54 @@
+<?php
+require_once('config.php');
+
+$connectionString = "mysql:host=". _MYSQL_HOST;
+
+if(defined('_MYSQL_PORT'))
+    $connectionString .= ";port=". _MYSQL_PORT;
+
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
+
+$pdo = NULL;
+try {
+    $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch (PDOException $erreur) {
+    echo('Erreur : '.$erreur->getMessage());
+}
+
+$request = $pdo->prepare("select * from users");
+
+$request -> execute();
+
+echo "<html>
+<h1>Liste des utilisateurs</h1>
+<table>
+";
+
+function generateHTMLRow($id, $name, $email) {
+    echo "<tr>";
+    echo "<td>".$id."</td><td>".$name."</td><td>".$email."</td>";
+    echo "</tr>";
+}
+
+generateHTMLRow('id','name','email');
+
+$resultat = $request->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($resultat as $row) {
+    generateHTMLRow($row['id'],$row['name'],$row['email']);
+}
+
+// /*print_r permet un affichage lisible des résultats,
+//  *<pre> rend le tout un peu plus lisible*/
+// echo '<pre>';
+// print_r($resultat);
+// echo '</pre>';
+
+/*** close the database connection ***/
+$pdo = null;
+
+?>
